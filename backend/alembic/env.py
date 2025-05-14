d599ba50ea7024fbf9fea64172947759a6ab55bf
rloss <rlossqianss@gmail.com>
2025-05-14 10:35:30 +0900

    env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -14,7 +14,7 @@ load_dotenv(dotenv_path=".env")
 config = context.config
 config.set_main_option(
     "sqlalchemy.url", os.getenv("SQLALCHEMY_DATABASE_URL")
-
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
